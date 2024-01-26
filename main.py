bcb39c876a337ef8e49d6bea575c2d1aba67a3f6
New <abvabv191@gmail.com>
2024-01-26 18:05:11 +0200

    3

--- main.py
+++ main.py
@@ -1,62 +1,51 @@
 from collections import UserDict
-from datetime import datetime
-
+from datetime import datetime, timedelta
 
 class Field:
     def __init__(self, value):
-        self.value = value
+        self._value = value
 
     @property
     def value(self):
         return self._value
 
     @value.setter
-    def value(self, value):
-        self._value = value
+    def value(self, new_value):
+        self._value = new_value
 
     def __str__(self):
-        return str(self.value)
-
+        return str(self._value)
 
 class Name(Field):
     pass
 
-
 class Phone(Field):
-    @Field.value.setter
-    def value(self, value):
-        if not isinstance(value, str) or len(value) != 10 or not value.isdigit():
-            raise ValueError("Phone number must contain exactly 10 digits.")
-        self._value = value  # corrected line
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate_phone()
 
+    def validate_phone(self):
+        if not isinstance(self._value, str) or len(self._value) != 10 or not self._value.isdigit():
+            raise ValueError("Phone number must contain exactly 10 digits.")
+
+    @Field.value.setter
+    def value(self, new_value):
+        self._value = new_value
+        self.validate_phone()
 
 class Birthday(Field):
+    def __init__(self, value=None):
+        super().__init__(value)
+
     @Field.value.setter
-    def value(self, value):
-        try:
-            datetime.strptime(value, '%Y-%m-%d')
-            self._value = value  # corrected line
-        except ValueError:
-            raise ValueError("Invalid birthday format. Use YYYY-MM-DD.")
-
-    def days_to_birthday(self):
-        if not self.value:
-            raise ValueError("Birthday not set.")
-
-        today = datetime.now()
-        bday = datetime.strptime(self.value, '%Y-%m-%d').replace(year=today.year)
-
-        if bday < today:
-            bday = bday.replace(year=today.year + 1)
-
-        return (bday - today).days
-
+    def value(self, new_value):
+        self._value = new_value
 
 class Record:
     def __init__(self, name, birthday=None):
         self.name = Name(name)
         self.phones = []
-        self.birthday = Birthday(birthday) if birthday else None
+        self.birthday = Birthday(birthday)
 
     def add_phone(self, phone):
         self.phones.append(Phone(phone))
@@ -67,15 +56,25 @@ class Record:
     def edit_phone(self, old_phone, new_phone):
         if not self.find_phone(old_phone):
             raise ValueError("Old phone number not found.")
+
         self.remove_phone(old_phone)
+
         self.add_phone(new_phone)
 
     def find_phone(self, phone):
         return next((p for p in self.phones if str(p) == str(phone)), None)
 
-    def __str__(self):
-        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}, birthday: {self.birthday}"
+    def days_to_birthday(self):
+        if self.birthday.value:
+            today = datetime.now().date()
+            next_birthday = datetime(today.year, self.birthday.value.month, self.birthday.value.day).date()
+            if today > next_birthday:
+                next_birthday = datetime(today.year + 1, self.birthday.value.month, self.birthday.value.day).date()
+            return (next_birthday - today).days
+        return None
 
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}, birthday: {self.birthday.value}"
 
 class AddressBook(UserDict):
     def add_record(self, record):
@@ -88,10 +87,7 @@ class AddressBook(UserDict):
         if name in self.data:
             del self.data[name]
 
-    def __iter__(self):
-        return self.iterator()
-
-    def iterator(self, n=10):
-        items = list(self.data.values())
-        for i in range(0, len(items), n):
-            yield items[i:i + n]
+    def iterator(self, N):
+        records = list(self.data.values())
+        for i in range(0, len(records), N):
+            yield records[i:i+N]
